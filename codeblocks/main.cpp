7aec973a2f0031a6407166ba80019bb063e3c416
Adrien Letellier <116633220+boalong@users.noreply.github.com>
2023-12-29 14:13:21 +0100

    Update main.cpp testing

--- codeblocks/main.cpp
+++ codeblocks/main.cpp
@@ -27,8 +27,65 @@ int main()
     double r = 0.02;
     double dividend = 0.01;
 
-    european_call test(spotprice, strikeprice, maturity, volatility, r);
-    european_put TEST(spotprice, strikeprice, maturity, volatility, r);
+    cout<<"Default values for the option's parameters:" << endl;
+    cout<<"===========================================" << endl << endl;
+    cout<<"Spot price: " << spotprice << endl;
+    cout<<"Strike price: " << strikeprice << endl;
+    cout<<"Maturity (years): " << maturity << endl;
+    cout<<"Volatility: " << volatility << endl;
+    cout<<"Risk-free interest rate: " << r << endl; 
+    cout<<"Annual dividend rate: " << dividend << endl << endl; 
+    
+
+    // European options testing
+
+    european_call EuropeanCall(spotprice, strikeprice, maturity, volatility, r);
+    european_put EureopeanPut(spotprice, strikeprice, maturity, volatility, r);
+
+    cout<<"European call, Black-Scholes: "<<EuropeanCall.price_BSM()<<endl;
+    cout<<"European call, Binomial Tree with 10000 timesteps: "<<EuropeanCall.price_BT(10000)<<endl;
+    cout<<"European call, Monte Carlo with 100 simulations, 10000 timesteps: "<<EuropeanCall.price_MC(100, 10000)<<endl;
+
+    cout<<"European put, Black-Scholes: "<<EuropeanPut.price_BSM()<<endl;
+    cout<<"European put, Binomial Tree with 10000 timesteps: "<<EuropeanPut.price_BT(10000)<<endl;
+    cout<<"European put, Monte Carlo with 100 simulations, 10000 timesteps: "<<EuropeanPut.price_MC(100, 10000)<<endl<<endl;
+
+
+    // Asian options testing
+
+    asian_call AsianCall(spotprice, strikeprice, maturity, volatility, r);
+    asian_put AsianPut(spotprice, strikeprice, maturity, volatility, r);
+
+    cout<<"Asian call, Black-Scholes: "<<AsianCall.price_BSM()<<endl;
+    cout<<"Asian call, Monte Carlo with 100 simulations, 10000 timesteps: "<<AsianCall.price_MC(100, 10000)<<endl;
+
+    cout<<"Asian put, Black-Scholes: "<<AsianPut.price_BSM()<<endl;
+    cout<<"Asian put, Monte Carlo with 100 simulations, 10000 timesteps: "<<AsianPut.price_MC(100, 10000)<<endl<<endl;
+
+    
+    // American options testing
+
+    american_put AmericanPut(spotprice, strikeprice, maturity, volatility, r);
+
+    cout<<"American put, Binomial Tree with 10000 timesteps: "<<AmericanPut.price_BT(10000)<<endl;
+    cout<<"American put, Monte Carlo with 100 simulations, 10000 timesteps: "<<AmericanPut.price_MC(100, 10000)<<endl<<endl;
+
+    
+    // European options with dividends testing
+
+    euro_dividend_put EuroDividendPut(spotprice, strikeprice, maturity, volatility, r, dividend);
+    euro_dividend_call EuroDividendCall(spotprice, strikeprice, maturity, volatility, r, dividend);
+
+    // Barrier options testing
+
+    barrier_call BarrierCall(spotprice, strikeprice, maturity, volatility, r, 1, 1, 75.0); //up-and-in call, barrier is 75
+    barrier_put BarrierPut(spotprice, strikeprice, maturity, volatility, r, 0, 1, 75.0); //up-and-out put, barrier is 75
+
+    // Lookback options testing
+    
+    lookback_call LookbackCall(spotprice, strikeprice, maturity, volatility, r);
+    lookback_put LookbackPut(spotprice, strikeprice, maturity, volatility, r);
+
 
     double P1 = test.price_BSM();
     double P2 = test.price_MC(100, 10000);
@@ -47,9 +104,6 @@ int main()
     cout<<Q2<<endl;
     cout<<Q3<<endl;
 
-    asian_call Test(spotprice, strikeprice, maturity, volatility, r);
-    asian_put tEST(spotprice, strikeprice, maturity, volatility, r);
-
     double p1 = Test.price_BSM();
     double p2 = Test.price_MC(100, 10000);
     double q1 = tEST.price_BSM();
@@ -60,21 +114,18 @@ int main()
     cout<<q1<<endl;
     cout<<q2<<endl;
 
-    american_put AmPut(spotprice, strikeprice, maturity, volatility, r);
 
-    cout << AmPut.price_BT(1000)<<endl;
-    cout << AmPut.price_MC(100, 1000)<<endl;
+    cout << AmericanPut.price_BT(1000)<<endl;
+    cout << AmericanPut.price_MC(100, 1000)<<endl;
 
-    euro_dividend_put test1(spotprice, strikeprice, maturity, volatility, r, dividend);
-    euro_dividend_call test2(spotprice, strikeprice, maturity, volatility, r, dividend);
 
     cout<<test1.price_BSM()<<endl;
     cout<<test1.price_MC(100, 1000)<<endl;
     cout<<test2.price_BSM()<<endl;
     cout<<test2.price_MC(100, 1000)<<endl;
 
-    barrier_call test3(spotprice, strikeprice, maturity, volatility, r, 1, 1, 75.0); //up-and-in call, barrier is 75
-    barrier_put test4(spotprice, strikeprice, maturity, volatility, r, 0, 1, 75.0); //up-and-out put, barrier is 75
+    barrier_call BarrierCall(spotprice, strikeprice, maturity, volatility, r, 1, 1, 75.0); //up-and-in call, barrier is 75
+    barrier_put BarrierPut(spotprice, strikeprice, maturity, volatility, r, 0, 1, 75.0); //up-and-out put, barrier is 75
 
     double R1 = test3.price_MC(100, 1000);
     double R2 = test4.price_MC(100, 1000);
@@ -82,8 +133,8 @@ int main()
     cout<<R1<<endl;
     cout<<R2<<endl;
 
-    lookback_call test5(spotprice, strikeprice, maturity, volatility, r);
-    lookback_put test6(spotprice, strikeprice, maturity, volatility, r);
+    lookback_call LookbackCall(spotprice, strikeprice, maturity, volatility, r);
+    lookback_put LookbackPut(spotprice, strikeprice, maturity, volatility, r);
 
     cout<<test5.price_MC(100, 1000)<<endl;
     cout<<test6.price_MC(100, 1000)<<endl;
